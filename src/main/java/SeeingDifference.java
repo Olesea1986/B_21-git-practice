f31af5c15f250b45a6d949b4d66f7e206ef01e8f
Olesea1986 <olesea86williams@gmail.com>
2021-02-03 22:26:22 -0600

    Added more work in Master branch SeeingDifference.

--- src/main/java/SeeingDifference.java
+++ src/main/java/SeeingDifference.java
@@ -3,4 +3,7 @@ public class SeeingDifference {
     // this is is a simple commit
     // new adds
     // master that movie5 does not know about
-}    // this is new line of work on movie5
+    // this is new line of work on movie5
+//I have more work to do
+
+}
